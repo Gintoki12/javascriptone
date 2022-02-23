f96ef93aa286e02ea441ed52b5bf8df6ad8288b2
Sakata Gintoki <sau.gat8602@protonmail.com>
2022-02-23 22:50:59 +0545

    Day 01-completed

---
+++ scripts/variable.js
@@ -0,0 +1,30 @@
+let name = 'Sakata'
+let number = 18
+let humpty
+console.log(typeof name)
+console.log(typeof number)
+console.log(typeof humpty)
+let tiger=true
+let sack =null
+console.log(typeof tiger)
+console.log(typeof sack)
+let harry, john, tommy, arthur
+let one=1
+two=2
+three=3
+four=4
+console.log(one, two, three, four)
+let firstname='Sakata'
+lastname='Gintoki'
+MaritalStatus='NotMarried'
+country='Nepal'
+Age=19
+console.log(firstname)
+console.log(lastname)
+console.log(MaritalStatus)
+console.log(country)
+console.log(Age)
+console.log(firstname, lastname, MaritalStatus, country, Age)
+let myAge='I am 25 years old.'
+yourAge= "You are 30 years old."
+console.log(myAge, yourAge)
