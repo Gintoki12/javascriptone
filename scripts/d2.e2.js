eb89a3e78b159d0f0cd58b25de570869abb92e26
Sakata Gintoki <sau.gat8602@protonmail.com>
2022-02-26 19:29:42 +0545

    small correction

--- scripts/d2.e2.js
+++ scripts/d2.e2.js
@@ -1,10 +1,11 @@
-let challenge = "The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another"
-console.log(challenge)//number 1
+let challenge = "The quote \'There is no exercise better for the heart than reaching down and lifting people up.\' by John Holmes teaches us to help one another"
+console.log(challenge)//number 1 backslash+' lets you insert apostrophe
 console.log("Love is not patronizing and charity isn't about pity, it is aobut love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead." )//number 2 
 let tenOne='10'
 let tenTwo=10
 tenOne2=parseInt(tenOne)
 let compareOne=typeof(tenOne2)==typeof(tenTwo)
 console.log(compareOne)//number 3 compared typeof '10' and 10; and made them equal using parseInt
+
 
 
