f99b3371f3f54679b9f932e42e1c2d17550c1abb
Sakata Gintoki <sau.gat8602@protonmail.com>
2022-03-03 16:29:40 +0545

    exercise 1 finished

---
+++ scripts/d3.js
@@ -0,0 +1,68 @@
+//Exercise 1
+let firstName = 'Sakata'
+let lastName = 'Gintoki'
+let country = 'India'
+let city = 'Bombay'
+let isMarried = false
+let year = 2022
+console.log(typeof firstName, typeof lastName, typeof country, typeof city, typeof isMarried, typeof year)
+// no1...checked the different data types with the help of type of
+
+
+console.log(typeof('10')==10)
+//no2... checked if typeof '10' equal to 10
+
+console.log(parseInt('9.8')==10)
+//no3... checked if parseInt('9.8') is equal to 10
+
+console.log(4>3)
+console.log(firstName=="Sakata")
+console.log(3!=4)
+//no4.i.. truthy statements
+
+let empty=null
+console.log(empty)
+console.log(lastName='Happy')
+console.log(4<3)
+//no4.ii.. falsy statements
+
+console.log(4>3)
+console.log(4>=3)
+console.log(4<3)
+console.log(4<=3)
+console.log(4==4)
+console.log(4===4)
+console.log(4!=4)
+console.log(4!==4)
+console.log(4!="4")
+console.log(4=='4')
+console.log(4==='4')
+console.log('python'.length!=='jargon'.length)
+//no5... some comparisons using comparison operators
+
+console.log(4>3&&10<12)
+console.log(4>3&& 10>12)
+console.log(4>3||10<12)
+console.log(4>3||10>12)
+console.log(!(4>3))
+console.log(!(4<3))
+console.log(!(false))
+console.log(!(4>3 &&10 <12))
+console.log(!(4>3&&10>12))
+console.log(!(4==='4'))
+console.log("There is no 'on' in both dragon and python.")
+//no6... some more comparisons using logical operators
+
+const now = new Date()
+console.log(now.getFullYear())
+console.log(now.getMonth()+1)
+console.log(now.getDate())
+console.log(now.getDay()+1)
+console.log(now.getHours())
+console.log(now.getMinutes())
+console.log(now.getTime())
+//no7 using date objects
+
+
+
+
