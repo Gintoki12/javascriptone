3ea2553b8a9d8b2b28c0e3cc10a9380656dc3b75
Gintoki12 <86060653+Gintoki12@users.noreply.github.com>
2022-03-06 00:11:12 +0545

    Exercise 1 done

--- scripts/d4.js
+++ scripts/d4.js
@@ -1,1 +1,47 @@
 //Exercise 1
+//Exercise 1
+let age = 18
+if(age >= 18){
+console.log('You are old enough to drive.')
+}
+else{
+console.log('You are not old enough to drive.')
+}
+//no1... couldn't use prompt in the online compilet but the results here..
+
+let myAge = 30
+let yourAge = 30
+if (myAge>yourAge){
+    console.log(`You are ${myAge-yourAge} years older than me.`)
+}
+else {
+    console.log(`You are ${yourAge-myAge} years older than me.`)
+}
+//no2... compared the ages and printed the statements according to them; could have added another else incase of same age but...no
+
+let a=4
+let b=3
+if(a>b){
+    console.log(`a is greater than b`)
+}
+else{
+    console.log(`b is greater than a`)
+}
+let c = (a>b)
+c
+? console.log('a is greater than b')
+: console.log('b is greater than a')
+//no3... compared two variables using if..else and ternary operators
+
+let n = 7
+if (n%2==0){
+    console.log('It is an even number')
+}
+else{
+    console.log('It is not an even number')
+}
+//no4... checked if a number is even or not.
+
+
+
+
