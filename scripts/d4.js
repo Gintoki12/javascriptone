0a2130ee86594380c18bcd4ddfbb21a2b9b5b5bf
Gintoki12 <86060653+Gintoki12@users.noreply.github.com>
2022-03-07 16:34:09 +0545

    End of day 4

--- scripts/d4.js
+++ scripts/d4.js
@@ -42,6 +42,74 @@ else{
 }
 //no4... checked if a number is even or not.
 
+//Exercise 2
+
+let score = 80
+if(score>=80&&score<=100){
+    console.log('A')
+}
+else if (score>=70&&score<=89){
+    console.log('B')
+}
+else if (score>=60&&score<=69){
+    console.log('C')
+}
+else if (score>=50&&score<=59){
+    console.log('D')
+}
+else if (score>=0&&score<=59){
+    console,log('F')
+}
+//no1.. compared the result and grades were given accordingly
+
+let season = "January"
+if (season=="September"||season=="October"||season=="November"){
+    console.log("The season is Autumn")
+}
+else if (season=='December'||season=='January'||season=='February'){
+    console.log('The season is Winter')
+}
+else if (season=='March'||season=='April'||season=='May'){
+    console.log('The season is Spring')
+}
+else if (season=='June'||season=='July'||season=='August'){
+    console.log('The season is Summer')
+}
+//no2.. taken the month as an input and compared them to their respective season
+
+let day = 'Tuesday'
+if (day=='Saturday'||day=="Sunday"){
+    console.log('It is a weekend')
+}
+else {
+    console.log('It is a weekday')
+}
+//no3.. checked the day whether it is a weekday or a weekend
+
+//Exercise 3
+
+
+let month='feBruary'
+let mon= month.toUpperCase()
+if (mon=='JANUARY'||mon=='MARCH'||mon=='MAY'||mon=='JULY'||mon=='AUGUST'||mon=='OCTOBER'||mon=='December'){
+    console.log('It has 31 days.')
+}
+else if (mon=='FEBRUARY'){
+    console.log('It has 28 days.')
+}
+else if (mon=='APRIL'||mon=='JUNE'||mon=='SEPTEMBER'||mon=='NOVEMBER'){
+    console.log('It has 30 days.')
+}
+//no1. no of days according to the month given
+
+
+
+
+
+
+
+
+
 
 
 
